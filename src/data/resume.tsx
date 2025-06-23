b87b72fdc7f8830ca4f2354bc29c0d5af76967dc
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2025-06-23 19:47:23 +0530

    data added

---
+++ src/data/resume.tsx
@@ -0,0 +1,245 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Priyanshu Ranjan",
+  initials: "PR",
+  url: "https://priyanshu.io",
+  location: "Ranchi, India",
+  locationLink: "https://www.google.com/maps/place/india",
+  description:
+    "Final year ECE undergrad at IIIT Ranchi. Tech enthusiast, Problem solver and Collaborative team player",
+  summary:
+    "I am a final-year B.Tech student at IIIT Ranchi, specializing in cutting-edge technology and passionate about innovation. With hands-on experience in projects and strong problem-solving skills, I am eager to contribute to impactful tech solutions. I thrive in collaborative environments and am driven to continuously learn and grow in the ever-evolving field of technology.",
+  avatarUrl: "me.jpg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Javascript",
+    
+    
+    "Postgres",
+    "Docker",
+    
+    
+    "C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "https://drive.google.com/file/d/1GPJ-X0Z_bLn-CzJhODRTI1zHgXZLHY6u/view?usp=sharing" , icon: NotebookIcon, label: "Resume" },
+  ],
+  contact: {
+    email: "hello@example.com",
+    tel: "+123456789",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/priyanshuranjan16",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/priyanshu-ranjan-27a74224a/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/priyanshu_1_6",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      LeetCode: {
+        name: "LeetCode",
+        url: "https://leetcode.com/blade_xd", 
+        icon: Icons.leetcode, 
+        navbar: true,
+      },
+
+      email: {
+        name: "Send Email",
+        url: "priyanshu.iiitr@gmail.com",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Alpixn Technologies Private Limited",
+      href: "https://www.linkedin.com/company/alpixn",
+      badges: [],
+      location: "Remote",
+      title: "Frontend Developer",
+      logoUrl: "/alpixnlogo.jpeg",
+      start: "May 2025",
+      end: "Present",
+      description:
+        "Made sign up and sign in dashboard with user interactive UI and many other features",
+    },
+   
+  ],
+  education: [
+    {
+      school: "Indian Institute of Information Technology, Ranchi",
+      href: "https://iiitranchi.ac.in/",
+      degree: "Bachelor of Technology in Electronics and Communication Engineering",
+      logoUrl: "/iiitlogo.png",
+      start: "2022",
+      end: "2026",
+    },
+    {
+      school: "Jawahar Vidya Mandir, Shyamali",
+      href: "http://www.jvmshyamali.com/",
+      degree: "Intermediate",
+      logoUrl: "/jvmlogo.png",
+      start: "2019",
+      end: "2021",
+    },
+    
+    
+  ],
+  projects: [
+    {
+      title: "  SensAi",
+      href: "https://sens-ai-five.vercel.app/",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://sens-ai-five.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/priyanshuranjan16/SensAI",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/sensai_bg.jpg",
+      video:
+        "",
+    },
+    {
+      title: "Fitron-AI",
+      href: "https://fitronai.vercel.app/",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://fitronai.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/priyanshuranjan16/Fitron-AI",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/fitron_bg.jpg",
+      video: "",
+    },
+    {
+      title: " Trimmr",
+      href: "https://trimrr-coral.vercel.app/",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://trimrr-coral.vercel.app/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/priyanshuranjan16/Trimrr",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/trimrr_bg.jpg",
+      video: "",
+    },
+    {
+      title: "Chessly",
+      href: "https://chessly-2.onrender.com/",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chessly-2.onrender.com/",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/priyanshuranjan16/chessly",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/chessly_bg.jpg",
+      video:
+        "",
+    },
+  ],
+ 
+} as const;
