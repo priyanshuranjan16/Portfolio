c0e07bf875e3076630f3a5828e97ce81f6c1b6dd
Priyanshu Ranjan <priyanshu.2022ug2005@iiitranchi.ac.in>
2025-11-17 10:59:23 +0530

    portfolio resume added

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -39,7 +39,7 @@ export const DATA = {
   
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    { href: "https://drive.google.com/file/d/1u62g9HXQjrs3hnNkLO2Gu4wLtPJZ8Gat/view?usp=sharing" , icon: NotebookIcon, label: "Resume" },
+    { href: "https://drive.google.com/file/d/1YemxBhYObf__xDH0R2RzmMDmKPiJOx07/view?usp=sharing" , icon: NotebookIcon, label: "Resume" },
   ],
   contact: {
     email: "hello@example.com",
